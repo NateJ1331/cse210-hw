c5af842c8a57dbe3fc6684ccdca468bad80cbf29
Nate J <nathan.apple131@gmail.com>
2023-04-21 15:01:47 -0600

    Made grade system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade precentage did you get? ");
+        string Grade = Console.ReadLine();
+        int IntGrade = int.Parse(Grade);
+        string LetterGrade = "";
+ 
+        if (IntGrade >= 90)
+        {
+            LetterGrade = "A";
+        }
+        
+        else if (IntGrade >= 80)
+        {
+            LetterGrade = "B";
+        }
+        
+        else if (IntGrade >= 70)
+        {
+            LetterGrade = "C";
+        }
+
+        else if (IntGrade >= 60)
+        {
+            LetterGrade = "D";
+        }
+
+        else if (IntGrade < 60)
+        {
+            LetterGrade = "F";
+        }
+
+
+        Console.WriteLine($"Your Grade is {LetterGrade}");
+
     }
 }
