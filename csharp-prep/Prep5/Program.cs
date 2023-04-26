43888edd4559b77dd46b091ab44ece3eb0e5483c
Nate J <nathan.apple131@gmail.com>
2023-04-26 14:45:21 -0600

    Wrote code using functions to get name and number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program! ");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    } 
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        string sNumber = Console.ReadLine();
+        int number = Int32.Parse(sNumber);
+        return number;
+    }
+
+    static double SquareNumber(double number)
+    {
+        double squaredNumber = number*number;
+    
+        return squaredNumber;
+    }
+    static void DisplayResult(string name, double square)
+    {
+        
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    
+    }    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name;
+        double number;
+        double square;
+    
+        DisplayWelcome();
+        name = PromptUserName();
+        number = PromptUserNumber();
+        square = SquareNumber(number); 
+        DisplayResult(name,square);
+
+        
     }
 }
