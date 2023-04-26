290b84eb03b3b7535058be16272a40089ca95aa3
Nate J <nathan.apple131@gmail.com>
2023-04-25 19:04:07 -0600

    Made a thing to generate a random number then wrote a loop to have them guess it

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+
+        int answer = rnd.Next(10);
+        int guess = -1;
+        
+
+
+        while (guess != answer)
+        {   
+            Console.WriteLine("Whats the Magic number? ");
+            string SGuess = guess.ToString();
+            SGuess = Console.ReadLine();
+            guess = Int32.Parse(SGuess);
+            
+            if (guess > answer)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            
+            else if (guess == answer)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+
+        }        
     }
 }
