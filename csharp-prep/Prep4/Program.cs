cb53c9657f92abcc3b84382d28c8ea0baa6626ca
Nate J <nathan.apple131@gmail.com>
2023-04-25 19:42:47 -0600

    made a progam to add to a list then give info based on that info

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>(); 
+        int input = -1;
+        double sum = 0;
+        Console.WriteLine("Enter a list of numbers type 0 when finished.");
+
+        while (input != 0)
+        {   
+            Console.Write("Enter a number: ");
+            string SInput = Console.ReadLine();
+            input = Int32.Parse(SInput);
+
+            numbers.Add(input);
+        }
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sum = numbers[i] + sum;
+        }
+        
+        double average = sum/(numbers.Count - 1);
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
