ffa2ad2e764ff26bbd8f374bddcb292fbd58fede
Nate J <nathan.apple131@gmail.com>
2023-05-01 22:30:35 -0600

    Made a program with classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
+
+    public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2017;
+        job1._endYear = 2023;
+
+        job2._company = "Microsoft";
+        job2._jobTitle = "HR";
+        job2._startYear = 2009;
+        job2._endYear = 2012;
+
+        job1.Display();
+        job2.Display();
+
     }
 }
