292ecdb9602a5345448bbaad31f0e0efc5edd7f0
Nate J <nathan.apple131@gmail.com>
2023-05-03 15:00:14 -0600

    Added Notes to project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+
+        //classes//
+        //1 and 2//
+            //Entry
+                //record the promt, response, and date
+                    //question
+                    //resonse
+                    //date
+                //Fuctions
+                    //Store(question/prompt,response,date)
+                    //FetchAString():string 
+            //Journal(has Entry)
+                //to store entries
+                    //List: Entry  
+                //Functions 
+                    //StorEentry(entry): void
+                    //FetchAllEntries(): List<Entry>
+            //Prompts
+                //Produce a predefined question (or prompt)
+
+                //GetRandomProompt(): string
+
+
         Console.WriteLine("Hello Develop02 World!");
     }
 }
