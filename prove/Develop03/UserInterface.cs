2fba8d83cc6f92d1099aac81da52a7287fbd0b75
Nate J <nathan.apple131@gmail.com>
2023-05-29 20:18:33 -0600

    saved the last stuff

--- prove/Develop03/UserInterface.cs
+++ prove/Develop03/UserInterface.cs
@@ -6,6 +6,8 @@ namespace Develop03
 {
     class UserInterface
     {
+        private string _quit = "";
+        private int _counter = 0;
         public string EnterReference()
         {
             string reference = "Heleman 5:12";
@@ -21,17 +23,69 @@ namespace Develop03
             return scripture;
         }
 
+        public int Counter(string[] words, int counter) 
+        {
+            for(int i = 0; i < words.Length; i++)
+                {
+                    if (words[i].Substring(0,1) == "_")
+                    {
+                        counter += 1;
+                    }                  
+                }
+
+            return counter;
+        }
+
         public void Display()
         {
             Scripture scripture = new Scripture();       
-            Word words = new Word();
-
-            Console.WriteLine($"{this.EnterReference()}:");
+            Word word = new Word();
+            
             string[] splitWords = scripture.GetWords();
+            
+                
 
-            foreach (string eachWord in splitWords)
+            Random rnd = new Random();
+            
+
+            while(_quit != "quit")
             {
-                Console.Write($"{words.GetWord(eachWord)} ");
+                _counter = 0;
+                int num1 = rnd.Next(splitWords.Length);
+                int num2 = rnd.Next(splitWords.Length);
+
+    
+                for(int i = 0; i < splitWords.Length; i++)
+                {
+                    if (i != num1 && i !=num2)
+                    {
+                        Console.Write($"{splitWords[i]} ");
+                    }
+
+                    else if (i == num1 || i == num2)
+                    {
+                        if(splitWords[i].Substring(0,1) == "_")
+                        { 
+                            Console.Write($"{splitWords[i]} ");
+                        }
+
+                        else
+                        {
+                            splitWords[i] = word.hide(splitWords[i]);
+                        }
+                    }
+                }
+
+                
+                _counter = this.Counter(splitWords,_counter);
+
+
+                //Testing Console.WriteLine($"\n\n{_counter}\n\n");
+                //Testing Console.WriteLine($"{splitWords.Length}");
+                Console.WriteLine("\n\nHit enter to continue or type quit to end");
+                _quit = Console.ReadLine();
+                if(_counter == splitWords.Length){break;}
+                Console.Clear();
             }
         }
         
