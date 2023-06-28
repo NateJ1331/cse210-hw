fad7d407eac8554fd21a5cba0389a2d5802ba194
Nate J <nathan.apple131@gmail.com>
2023-06-27 20:11:30 -0600

    Tested some stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
+    class Test
+    {
+        public Test _test = new Test();
+
+        string a;
+        public void yourmom()
+        {
+            _test.a = "bruh";
+
+            Console.WriteLine(_test.a);
+            
+        }
+
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello SandBox World!!!");
+        Test Test = new Test();
+
+        Test._test.yourmom();
     }
 }
